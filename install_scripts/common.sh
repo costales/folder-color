c326a93e1af6efb10b066a29aae2e169c5efbebe
Costales <costales.marcos@gmail.com>
2020-12-24 18:03:13 +0100

    Issue #11: Fixed depedence gvfs-bin in 20.10+

--- install_scripts/common.sh
+++ install_scripts/common.sh
@@ -16,7 +16,7 @@ sed -i '1d' ../debian/install
 sed -i 's/Upstream-Name: folder-color/Upstream-Name: folder-color-common/' ../debian/copyright
 sed -i 's/Source: folder-color/Source: folder-color-common/' ../debian/control
 sed -i 's/Package: folder-color/Package: folder-color-common/' ../debian/control
-sed -i 's/python3-nautilus, nautilus, /gvfs-bin, /' ../debian/control
+sed -i 's/python3-nautilus, nautilus, //' ../debian/control
 sed -i 's/Breaks: .*/Breaks: folder-color (<< 0.0.50), folder-color-nemo (<< 0.0.50), folder-color-caja (<< 0.0.50)/' ../debian/control
 sed -i 's/Replaces: .*/Replaces: folder-color (<< 0.0.50), folder-color-nemo (<< 0.0.50), folder-color-caja (<< 0.0.50)/' ../debian/control
 sed -i 's/Folder Color for Nautilus/Folder Color Library/' ../debian/control
