b0fb3171ed1c60ad0f75dac6048a50814a23d718
Costales <costales.marcos@gmail.com>
2026-04-05 09:49:01 +0200

    updated install scripts

--- install-scripts/caja.sh
+++ install-scripts/caja.sh
@@ -7,12 +7,12 @@ rm ../README.md
 sed -i 's/python3-nautilus, nautilus, /python3-caja, caja, /' ../debian/control
 
 # folder-color to folder-color-nemo
-find . -type f -name "*.txt" | xargs sed -i 's/folder-color/folder-color-caja/g'
+find . -type f | xargs sed -i 's/folder-color/folder-color-caja/g'
 
 # nautilus to nemo
-find . -type f -name "*.txt" | xargs sed -i 's/nautilus/caja/g'
-find . -type f -name "*.txt" | xargs sed -i 's/Nautilus/Caja/g'
-find . -type f -name "*.txt" | xargs sed -i 's/metadata::custom-icon-name/metadata::custom-icon/g'
+find . -type f | xargs sed -i 's/nautilus/caja/g'
+find . -type f | xargs sed -i 's/Nautilus/Caja/g'
+find . -type f | xargs sed -i 's/metadata::custom-icon-name/metadata::custom-icon/g'
 
 # Delete myself
 rm -rf ../install-scripts
--- install-scripts/nautilus.sh
+++ install-scripts/nautilus.sh
@@ -3,13 +3,5 @@
 rm -rf ../.git
 rm ../README.md
 
-# po
-sed -i 's/folder_i18n/folder-color/' ../po/POTFILES.in
-sed -i 's/folder_path/extension-GTK4/' ../po/POTFILES.in
-sed -i 's/folder_i18n/folder-color/' ../extension-GTK3/folder-color.py
-sed -i 's/folder_i18n/folder-color/' ../extension-GTK4/folder-color.py
-
-# myself
+# Delete myself
 rm -rf ../install-scripts
-
-echo "Done"
