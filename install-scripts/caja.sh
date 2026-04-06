75f64039994cbff5a9cc20b76cd039e7a254678c
Costales <costales.marcos@gmail.com>
2026-04-06 15:28:50 +0200

    Lintian

--- install-scripts/caja.sh
+++ install-scripts/caja.sh
@@ -8,9 +8,9 @@ sed -i "s/metadata::custom-icon-name/metadata::custom-icon/g" ../extension/GTK3.
 sed -i "s/metadata::custom-icon-name/metadata::custom-icon/g" ../extension/GTK4.py
 
 # Build
-find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/folder-color/folder-color-caja/g'
-find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/nautilus/caja/g'
-find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/Nautilus/Caja/g'
+find .. -type f -print0 | xargs -0 sed -i 's/folder-color/folder-color-caja/g'
+find .. -type f -print0 | xargs -0 sed -i 's/nautilus/caja/g'
+find .. -type f -print0 | xargs -0 sed -i 's/Nautilus/Caja/g'
 
 # Myself
 rm -rf ../install-scripts
