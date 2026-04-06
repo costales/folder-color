56afd0d36f76e2c9f4d7e97e730db1ab121ffd9f
Costales <costales.marcos@gmail.com>
2026-04-06 15:32:04 +0200

    Lintian

--- install-scripts/nemo.sh
+++ install-scripts/nemo.sh
@@ -5,6 +5,7 @@ rm ../README.md
 
 # Build
 find .. -type f -print0 | xargs -0 sed -i 's/folder-color/folder-color-nemo/g'
+find .. -type f -print0 | xargs -0 sed -i 's/python3-nautilus/python-nemo/g'
 find .. -type f -print0 | xargs -0 sed -i 's/nautilus/nemo/g'
 find .. -type f -print0 | xargs -0 sed -i 's/Nautilus/Nemo/g'
 
