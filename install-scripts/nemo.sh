05f5aeca849337c60c7e2ea358a09b1d4a398885
Costales <costales.marcos@gmail.com>
2026-04-06 15:27:10 +0200

    Lintian

--- install-scripts/nemo.sh
+++ install-scripts/nemo.sh
@@ -4,9 +4,9 @@ rm -rf ../.git
 rm ../README.md
 
 # Build
-find . -type f -name "*.txt" -print0 | xargs -0 sed -i 's/folder-color/folder-color-nemo/g'
-find . -type f -name "*.txt" -print0 | xargs -0 sed -i 's/nautilus/nemo/g'
-find . -type f -name "*.txt" -print0 | xargs -0 sed -i 's/Nautilus/Nemo/g'
+find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/folder-color/folder-color-nemo/g'
+find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/nautilus/nemo/g'
+find .. -type f -name "*.txt" -print0 | xargs -0 sed -i 's/Nautilus/Nemo/g'
 
 # Myself
 rm -rf ../install-scripts
